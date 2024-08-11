2214ced0c1ce025f883230d0031237dcb195ca0e
Tony Sunny <tonysunnymails@gmail.com>
2024-08-11 20:01:19 +0530

    Update vim config with plugins

--- setup-macos.sh
+++ setup-macos.sh
@@ -1,5 +1,19 @@
 #!/usr/bin/env bash
 
-brew install kitty fnm startship zsh-syntax-highlighting zsh-autosuggestions
+brew install \
+        fnm \
+        font-fira-code-nerd-font \
+        kitty \
+        startship \
+        vim \
+        zsh-autosuggestions \
+        zsh-syntax-highlighting
+
+cp macos/vimrc ~/.vimrc
+curl -fLo ~/.var/app/io.neovim.nvim/data/nvim/site/autoload/plug.vim --create-dirs \
+https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+vim +PlugInstall +qall 2>&1 /dev/null
+
 cp macos/zshrc ~/.zshrc
+
 cp macos/kitty.conf ~/.config/kitty/kitty.conf
